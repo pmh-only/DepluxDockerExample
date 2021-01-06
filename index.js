52297c8277dc2d319bd011e5cc14e4cf99e2cb27
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2021-01-06 22:17:40 +0900

    enviroment variables

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express()
 app.use((req, res) => {
   res
     .set('Content-Type', 'text/html; charset=UTF-8')
-    .send('HOI!! your ip is: ' + req.ip + '!!<hr />hosted by Deplux')
+    .send(
+      'HOI!! your ip is: ' + req.ip + '!!<hr />' +
+      'hosted by Deplux<br />' +
+      'Envvars: <ul>' + Object.keys(process.env).reduce((prev, curr) => `${prev}\n<li>${curr} : ${process.env[curr]}</li>`, '') + '</ul>')
 })
 
 app.listen(process.env.PORT)
